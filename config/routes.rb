2cfab8ede0934fa1410371d25b3078b567bc57bb
Nk04 <n.kazuma1998@gmail.com>
2020-08-27 15:38:40 +0900

    ルートの記述とDB:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  } 
+
   root "complaints#index"
   resources :complaints, only: :index
+  resources :user, only: [:new, :create, :destroy]
 end
