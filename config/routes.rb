82f015cd354034f3bd8945b373391cdfc0569bd7
Nk04 <n.kazuma1998@gmail.com>
2020-08-27 16:53:40 +0900

    ルート修正view記述

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   root "complaints#index"
   resources :complaints, only: :index
-  resources :user, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :destroy]
 end
